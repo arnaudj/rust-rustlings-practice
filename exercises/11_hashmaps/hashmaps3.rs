369cd767e7f0c8a3110505f2facfd5fa3899da37
Arnaud J <github@arnaud.dev>
2024-05-27 21:32:34 +0200

    feat: complete exercises 11_hashmaps

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -5,17 +5,15 @@
 // Example: England,France,4,2 (England scored 4 goals, France 2).
 //
 // You have to build a scores table containing the name of the team, the total
-// number of goals the team scored, and the total number of goals the team 
-// conceded. One approach to build the scores table is to use a Hashmap. 
-// The solution is partially written to use a Hashmap, 
+// number of goals the team scored, and the total number of goals the team
+// conceded. One approach to build the scores table is to use a Hashmap.
+// The solution is partially written to use a Hashmap,
 // complete it to pass the test.
 //
 // Make me pass the tests!
 //
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -28,6 +26,18 @@ struct Team {
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
+    let mut update_team = |name: String, scored: u8, conceded: u8| {
+        scores
+            .entry(name)
+            .and_modify(|t| {
+                t.goals_scored += scored;
+                t.goals_conceded += conceded;
+            })
+            .or_insert(Team {
+                goals_scored: scored,
+                goals_conceded: conceded,
+            });
+    };
 
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
@@ -35,11 +45,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded by team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        update_team(team_1_name, team_1_score, team_2_score);
+        update_team(team_2_name, team_2_score, team_1_score);
     }
     scores
 }
