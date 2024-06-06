807a922a5a631d1ae2275aba5302c7633f5a5306
Arnaud J <github@arnaud.dev>
2024-06-06 22:29:38 +0200

    feat: complete exercise comprehensive-rust/lifetimes/exercise

--- exercises/16_lifetimes/lifetimes4.rs
+++ exercises/16_lifetimes/lifetimes4.rs
@@ -117,14 +117,42 @@ fn unpack_tag(tag: u64) -> Result<(u64, WireType), Error> {
     Ok((field_num, wire_type))
 }
 
-/// Parse a field, returning the remaining bytes
+/// Parse a field, returning the Field and remaining bytes
 fn parse_field(data: &[u8]) -> Result<(Field, &[u8]), Error> {
     let (tag, remainder) = parse_varint(data)?;
     let (field_num, wire_type) = unpack_tag(tag)?;
     let (fieldvalue, remainder) = match wire_type {
-        _ => todo!("Based on the wire type, build a Field, consuming as many bytes as necessary."),
+        WireType::Varint => {
+            let (value, remainder) = parse_varint(remainder)?;
+            (FieldValue::Varint(value), remainder)
+        }
+        WireType::Len => {
+            let (len, remainder) = parse_varint(remainder)?;
+            let len: usize = len.try_into()?;
+            if remainder.len() < len {
+                return Err(Error::UnexpectedEOF);
+            }
+            let (value, remainder) = remainder.split_at(len);
+            (FieldValue::Len(value), remainder)
+        }
+        WireType::I32 => {
+            // from solution:
+            if remainder.len() < 4 {
+                return Err(Error::UnexpectedEOF);
+            }
+            let (value, remainder) = remainder.split_at(4);
+            // Unwrap error because `value` is definitely 4 bytes long.
+            let value = i32::from_le_bytes(value.try_into().unwrap());
+            (FieldValue::I32(value), remainder)
+        }
     };
-    todo!("Return the field, and any un-consumed bytes.")
+    Ok((
+        Field {
+            field_num,
+            value: fieldvalue,
+        },
+        remainder,
+    ))
 }
 
 /// Parse a message in the given data, calling `T::add_field` for each field in
@@ -154,9 +182,35 @@ struct Person<'a> {
     phone: Vec<PhoneNumber<'a>>,
 }
 
-// TODO: Implement ProtoMessage for Person and PhoneNumber.
+impl<'a> ProtoMessage<'a> for PhoneNumber<'a> {
+    fn add_field(&mut self, field: Field<'a>) -> Result<(), Error> {
+        match field.field_num {
+            1 => self.number = field.value.as_string()?,
+            3 => self.type_ = field.value.as_string()?,
+            _ => return Err(Error::UnexpectedWireType),
+        }
+        Ok(())
+    }
+}
 
+impl<'a> ProtoMessage<'a> for Person<'a> {
+    fn add_field(&mut self, field: Field<'a>) -> Result<(), Error> {
+        match field.field_num {
+            1 => self.name = field.value.as_string()?,
+            2 => self.id = field.value.as_u64()?,
+            3 => self.phone.push(parse_message(field.value.as_bytes()?)?),
+            _ => return Err(Error::UnexpectedWireType),
+        }
+        Ok(())
+    }
+}
+
+use std::panic;
 fn main() {
+    panic::set_hook(Box::new(|panic_info| {
+        // main screen turn on:
+        println!("panic occurred: {panic_info}");
+    }));
     let person: Person = parse_message(&[
         0x0a, 0x07, 0x6d, 0x61, 0x78, 0x77, 0x65, 0x6c, 0x6c, 0x10, 0x2a, 0x1a, 0x16, 0x0a, 0x0e,
         0x2b, 0x31, 0x32, 0x30, 0x32, 0x2d, 0x35, 0x35, 0x35, 0x2d, 0x31, 0x32, 0x31, 0x32, 0x12,
@@ -166,4 +220,31 @@ fn main() {
     ])
     .unwrap();
     println!("{:#?}", person);
+}
+
+// from solution:
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn as_string() {
+        assert!(FieldValue::Varint(10).as_string().is_err());
+        assert!(FieldValue::I32(10).as_string().is_err());
+        assert_eq!(FieldValue::Len(b"hello").as_string().unwrap(), "hello");
+    }
+
+    #[test]
+    fn as_bytes() {
+        assert!(FieldValue::Varint(10).as_bytes().is_err());
+        assert!(FieldValue::I32(10).as_bytes().is_err());
+        assert_eq!(FieldValue::Len(b"hello").as_bytes().unwrap(), b"hello");
+    }
+
+    #[test]
+    fn as_u64() {
+        assert_eq!(FieldValue::Varint(10).as_u64().unwrap(), 10u64);
+        assert!(FieldValue::I32(10).as_u64().is_err());
+        assert!(FieldValue::Len(b"hello").as_u64().is_err());
+    }
 }
