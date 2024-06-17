0894128e5c70db790721def2ad1f4d286d748153
Arnaud J <github@arnaud.dev>
2024-06-17 23:47:58 +0200

    feat: complete exercise comprehensive-rust/smart-pointers

--- exercises/19_smart_pointers/exercise.rs
+++ exercises/19_smart_pointers/exercise.rs
@@ -1,6 +1,8 @@
 // https://google.github.io/comprehensive-rust/smart-pointers/exercise.html
 
+use std::cmp::Ordering;
 /// A node in the binary tree.
+///
 #[derive(Debug)]
 struct Node<T: Ord> {
     value: T,
@@ -40,7 +42,50 @@ impl<T: Ord> BinaryTree<T> {
     }
 }
 
-// Implement `new`, `insert`, `len`, and `has` for `Subtree`.
+impl<T: Ord> Subtree<T> {
+    fn new() -> Self {
+        Self(None)
+    }
+
+    fn insert(&mut self, value: T) {
+        match &mut self.0 {
+            None => self.0 = Some(Box::new(Node::new(value))),
+            Some(n) => match value.cmp(&n.value) {
+                Ordering::Less => n.left.insert(value),
+                Ordering::Equal => {}
+                Ordering::Greater => n.right.insert(value),
+            },
+        }
+    }
+
+    fn has(&self, value: &T) -> bool {
+        match &self.0 {
+            None => false,
+            Some(n) => match value.cmp(&n.value) {
+                Ordering::Less => n.left.has(value),
+                Ordering::Equal => true,
+                Ordering::Greater => n.right.has(value),
+            },
+        }
+    }
+
+    fn len(&self) -> usize {
+        match &self.0 {
+            None => 0,
+            Some(n) => n.left.len() + n.right.len() + 1,
+        }
+    }
+}
+
+impl<T: Ord> Node<T> {
+    fn new(value: T) -> Self {
+        Node {
+            value,
+            left: Subtree::new(),
+            right: Subtree::new(),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
